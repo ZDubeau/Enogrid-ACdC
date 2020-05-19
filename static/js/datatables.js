ee9c15b761e112f55339d448dddccf7fb28aed12
Zahra Dubeau <zdub@dubz.com>
2020-05-19 09:06:42 +0200

    Add new option as all files

--- static/js/datatables.js
+++ static/js/datatables.js
@@ -25,15 +25,15 @@ $(document).ready(function () {
     "order": [[0, "desc"]],
 
     "fnRowCallback": function (nRow, aData) {
-      var status = aData[5];
+      var status = Math.round(aData[5]);
       switch (status) {
-        case "100":
+        case 100:
           $('td:eq(4)', nRow).html(validate);
           break;
-        case "80":
+        case 80:
           $('td:eq(4)', nRow).html(hourglass);
           break;
-        case "50":
+        case 50:
           $('td:eq(4)', nRow).html(not_started);
           break;
         default:
@@ -82,21 +82,22 @@ $(document).ready(function () {
 
   $('#dataTableProjectEditFiles').DataTable({
     "columnDefs": [
-      { "width": "13px", "targets": 0 },
+      { "visible": false, "targets": 0 },
       { "visible": false, "targets": 1 },
       { "width": "35px", "targets": 2 },
-      { "width": "40px", "targets": 3 },
+      { "width": "30px", "targets": 3 },
       { "width": "40px", "targets": 4 },
       { "width": "35px", "targets": 5 },
-      { "width": "50px", "targets": 6 },
+      { "width": "40px", "targets": 6 },
       { "width": "50px", "targets": 7 },
-      { "width": "40px", "targets": 8 },
-      { "width": "35px", "targets": 9 }
+      { "width": "50px", "targets": 8 },
+      { "width": "35px", "targets": 9 },
+      { "width": "30px", "targets": 10 }
     ],
     "order": [[0, "desc"]],
     "fnRowCallback": function (nRow, aData) {
       var id = aData[0];
-      if (aData[2] == "Analysé") {
+      if (aData[3] == "Analysé") {
         $('td:eq(7)', nRow).html(download);
         $('td:eq(7)', nRow).click(function () {
           window.location.href = '/download_file/' + id;
@@ -105,12 +106,57 @@ $(document).ready(function () {
         $('td:eq(8)', nRow).click(function () {
           window.location.href = '/delete_file/' + id;
         });
-      } else if (aData[2] == "Erreur") {
+      } else if (aData[3] == "Erreur") {
         $('td:eq(8)', nRow).html(cancel);
         $('td:eq(8)', nRow).click(function () {
           window.location.href = '/delete_file/' + id;
         });
       }
+    }
+  });
+
+  $('#dataTableProjectEditAllFiles').DataTable({
+    "columnDefs": [
+      { "visible": false, "targets": 0 },
+      { "visible": false, "targets": 1 },
+      { "width": "30px", "targets": 2 },
+      { "width": "30px", "targets": 3 },
+      { "width": "30px", "targets": 4 },
+      { "width": "30px", "targets": 5 },
+      { "width": "20px", "targets": 6 },
+      { "width": "25px", "targets": 7 },
+      { "width": "25px", "targets": 8 }
+    ],
+    "columns": [
+      { title: "" },
+      { title: "" },
+      { title: "Name" },
+      { title: "Type" },
+      { title: "kWh_normalisé" },
+      { title: "kWh_standardisé" },
+      { title: "Delta (ppm)" },
+      { title: "Télécharger (nm)" },
+      { title: "Télécharger (sd)" }
+    ],
+    "order": [[0, "desc"]],
+    "fnRowCallback": function (nRow, aData) {
+      var ppm = aData[6];
+      switch (ppm) {
+        case "NaN":
+          $('td:eq(4)', nRow).html("");
+          break;
+        default:
+          ppm = Math.round(ppm, 0);
+          $('td:eq(4)', nRow).html(ppm);
+      }
+      $('td:eq(5)', nRow).html(download);
+      $('td:eq(5)', nRow).click(function () {
+        window.location.href = '/download_file/' + id;
+      });
+      $('td:eq(6)', nRow).html(download);
+      $('td:eq(6)', nRow).click(function () {
+        window.location.href = '/download_file/' + id;
+      });
     }
   });
 
